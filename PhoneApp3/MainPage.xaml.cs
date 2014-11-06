05670f22ee028194ed80b5629a3578a5175f5a73
timothy <tbradford16@my.whitworth.edu>
2014-11-05 18:24:20 -0800

    Initial commit

---
+++ PhoneApp3/MainPage.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp3.Resources;
+
+namespace PhoneApp3
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        Math_Stuff calc = new Math_Stuff();
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void zero_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "0";
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text = "0";
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "1";
+        }
+
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "2";
+        }
+
+        private void three_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "3";
+        }
+
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "4";
+        }
+
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "5";
+        }
+
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "6";
+        }
+
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "7";
+        }
+
+        private void eight_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "8";
+        }
+
+        private void nine_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "9";
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "+";
+        }
+
+        private void sub_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "-";
+        }
+
+        private void mult_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "*";
+        }
+
+        private void devide_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text += "/";
+        }
+
+        private void equal_Click(object sender, RoutedEventArgs e)
+        {
+            tbox.Text = calc.calculate(tbox.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ PhoneApp3/Math_Stuff.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneApp3
+{
+    class Math_Stuff
+    {
+        public string calculate(string num)
+        {
+            int count = 0, inserter = 0;
+
+            double A_num = 0, num0 = 0, A_num1=0;
+            List<string> operators = new List<string>();
+            List<string> numbers = new List<string>();
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                bool canConvert = double.TryParse(num[i].ToString(), out num0);//This will step through the string char by char and set a bool to indicate if it can convert to a double or not
+
+                if (canConvert == false)
+                {
+                    operators.Add(num[i].ToString());// This stores each operator in the order that the user imputed
+                    count++;// this will happen every set of digits i.e after the first number is completely transferred into the place holder. the first place holder will be 0 and the second 1 and so on...
+                    inserter = 0;// I needed this because I want to insert a new place holder for my list and add to that existing list after word 
+                }
+                else if (canConvert == true && inserter == 0) // if canConvert == true
+                {
+                    numbers.Insert(count, num[i].ToString());// this will create a new place holder for the first digit of a number. The list will increment after every operator
+                    inserter++;
+                }
+                else if (canConvert == true)
+                {
+                    numbers[count] += num[i].ToString();//this adds the rest of the digits to the number char by char
+                }
+            }
+            // the numbers list will hold all the numbers in order of them being typed i.e if the first number typed is 100 then numbers[0] = 100 and so on...
+            // the operators list will hold each operator in order that it is input. This assumes there will always be one more number than operator.
+            for (int q = 0; q < operators.Count(); q += 2)//this will not do order of operations but will read and compute the equation left to right
+            {
+                A_num1 = Convert.ToDouble(numbers[q + 1]);// A_num1 will be used to manipulate the A_num amount
+                A_num = Convert.ToDouble(numbers[q]);// this will be the total amount
+
+                if (operators[q] == "/")// this will check to see which operator is being used
+                {
+                    A_num /= A_num1;
+                    numbers[q + 1] += A_num.ToString();
+                }
+                else if (operators[q] == "*")
+                {
+                    A_num *= A_num1;
+                    numbers[q + 1] += A_num.ToString();
+                }
+                else if (operators[q] == "-")
+                {
+                    A_num -= A_num1;
+                    numbers[q + 1] += A_num.ToString();
+                }
+                else if (operators[q] == "+")
+                {
+                    A_num += A_num1;
+                    numbers[q + 1] += A_num.ToString();
+                }
+            }
+
+            return A_num.ToString();//This returns the total amount after calculations are done
+        }
+    }
+}
